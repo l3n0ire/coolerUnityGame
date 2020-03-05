e319c9f18af8123d6022ca70c724e52edfc79d61
Colin Lin <colincool100@gmail.com>
2020-03-05 18:43:58 -0500

    switch
    woah

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,14 +6,20 @@ public class player : MonoBehaviour
 {
     public int velocity = 5;
     public int jump =500;
-    public GameObject Ob;
+    public GameObject Ob1;
+    public GameObject Ob2;
+    public string currentName;
     bool inAir=false;
-    Rigidbody rb;
+    public bool isOb1=true;
+    public Rigidbody rb1;
+    public Rigidbody rb2;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb =Ob.GetComponent<Rigidbody>();
+        currentName=Ob1.name;
+        
+
 
         //rb.AddForce(new Vector3(0,500,0));
     }
@@ -24,10 +30,13 @@ public class player : MonoBehaviour
             inAir=false;
 
         }
-        if(collision.gameObject.name=="powerUp"){
-            velocity*=2;
-            jump*=2;
-            Destroy(collision.gameObject);
+        if(collision.gameObject.tag =="powerUp"){
+            if(collision.gameObject.name=="speed"){
+                  velocity*=2;
+                  jump*=2;
+                
+                Destroy(collision.gameObject);
+            }
         }
         
     }
@@ -39,15 +48,34 @@ public class player : MonoBehaviour
         
         Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
 
-        if(Ob.name == "Player")
-        {
-            transform.Translate(move * velocity * Time.deltaTime, Space.World);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), 0.15F);
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            isOb1=!isOb1;
+
         }
 
+        if(isOb1 )
+        {
+            Ob1.transform.Translate(move * velocity * Time.deltaTime, Space.World);
+            if(move!=Vector3.zero){
+                Ob1.transform.rotation = Quaternion.Slerp(Ob1.transform.rotation, Quaternion.LookRotation(move), 0.15F);
+
+            }
+       }
+       else if(!isOb1 )
+        {
+            Ob2.transform.Translate(move * velocity * Time.deltaTime, Space.World);
+            Ob2.transform.rotation = Quaternion.Slerp(Ob2.transform.rotation, Quaternion.LookRotation(move), 0.15F);
+       }
+
         if(Input.GetKeyDown(KeyCode.RightShift) && !inAir){
-            rb.AddForce(new Vector3(0,jump,0));
-            inAir=true;
+            if(isOb1){
+                rb1.AddForce(new Vector3(0,jump,0));
+                inAir=true;
+            }
+            else if(!isOb1){
+                rb2.AddForce(new Vector3(0,jump,0));
+                inAir=true;
+            }
         }
     }
 }
